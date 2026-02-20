cde4b4da56ae226e3e4c2ce42ef88da63c37ef3e
yash-rajput12 <yashankrajput45@gmail.com>
2026-02-20 17:15:08 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0)return false;
+        }
+        return true;
+            
+    }
+}
+ 
