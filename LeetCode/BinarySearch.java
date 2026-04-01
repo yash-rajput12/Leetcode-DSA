97ae92eead461fa7fe28a8fe49f9a8f0efd89bea
yash-rajput12 <yashankrajput45@gmail.com>
2026-04-01 16:13:21 +0530

    Create BinarySearch.java

---
+++ LeetCode/BinarySearch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int lo=0;
+        int hi=n-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]>target){
+                hi=mid-1;
+            }
+            else if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
